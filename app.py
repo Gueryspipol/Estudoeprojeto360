f83292e32d7761f97ba0e5a00ea48b388d958bd4
Gueryspipol <visantos0811@gmail.com>
2026-08-19 17:35:16 -0300

    Correção do app.py

--- app.py
+++ app.py
@@ -1,5 +1,22 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Estudoeprojeto360
+    page_title="Estudoeprojeto360",
+    layout="wide"
 )
+
+st.title("Estudoeprojeto360")
+
+st.subheader(
+    "Automação de geração de apresentações corporativas"
+)
+
+arquivo = st.file_uploader(
+    "Selecione a matriz Excel",
+    type=["xlsx"]
+)
+
+if arquivo:
+    st.success(
+        "Arquivo carregado com sucesso!"
+    )
