c685b96bbff3f5721d80eec93c25bb7af0a64b46
Gueryspipol <visantos0811@gmail.com>
2026-08-19 17:54:21 -0300

    Add function to read Excel file and return sheet info

--- etapas/etapas/teste.py
+++ etapas/etapas/teste.py
@@ -1,2 +1,10 @@
-def teste():
-    return "Robô conectado!"
+import pandas as pd
+
+def ler_matriz(arquivo):
+
+    excel = pd.ExcelFile(arquivo)
+
+    return {
+        "abas": excel.sheet_names,
+        "quantidade_abas": len(excel.sheet_names)
+    }
