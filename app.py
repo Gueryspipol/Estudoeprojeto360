56d0321267c3c37b29d69b2d20f4ca651ed0ac99
Gueryspipol <visantos0811@gmail.com>
2026-08-19 17:29:09 -0300

    Correção do app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Estudoeprojeto360",
-    layout="wide"
-)
-
-st.title("Estudoeprojeto360")
-
-st.subheader(
-    "Automação de gera
+    page_title="Estudoeprojeto360
